68ebf0b2b56d7793efbc99e75e161dbdf23b505c
OlenchenkoVladimir <89122417+OlenchenkoVladimir@users.noreply.github.com>
2022-09-12 22:03:09 +0300

    modify LastDigiOf.hs

--- LastDigitOf.hs
+++ LastDigitOf.hs
@@ -1,24 +1,27 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-
-
 module LastDigitOf where
-import Data.List (unfoldr)
 
+import Data.List()
+import Data.Text (splitOn)
 
-lastDigitOf :: Integer -> Integer -> Integer
-lastDigitOf 0 0 = 1
-lastDigitOf 0 1 = 0
-lastDigitOf x y = last $ digs $ x ^ y
+-- <func append> -> <empty unit> -> <base elem> -> <exponential> -> <res>
+fastContAppendOp :: Integral a => (b -> b -> b) -> b -> b -> a -> b
+fastContAppendOp _ e _ 0 = e
+fastContAppendOp f e a k
+    | mod k 2 == 1 = f a (fastContAppendOp f e a (k - 1))
+    | otherwise = fastContAppendOp f e (f a a) (div k 2)
 
+-- Optimized specified FCOODCMWDBAE algorithm on Monoid <N+, *> with mod operation
+-- <base> -> <exp> -> <moder> -> <res>
+fastExpModOpti :: Integral a => a -> a -> a -> a
+fastExpModOpti a k m = fastContAppendOp (\a b -> mod (a * b) m) 1 a k
 
-digs :: Integral x => x -> [x]
-digs 0 = []
-digs x = digs (x `div` 10) ++ [x `mod` 10]
+-- Problem 1193 Specified algorithm
+-- <A> -> <M> -> <N> -> <res>
+passwordAtNthDay :: Int -> Int -> Int -> Int
+passwordAtNthDay a m n = mod (fastExpModOpti a n m) m
 
-digs' :: Integer -> [Integer]
-digs' = unfoldr (\b -> if b == 0
-        then Nothing
-        else Just (b `mod` 10,
-                  b `div` 10))
-diggg :: Integral b => b -> [b]
-diggg x = scanl mod x []
+splitAndExtInt :: String -> [Int]
+splitAndExtInt s = map read $ splitOn " " s
+
+lastDigit :: Integer -> Integer -> Integer
+lastDigit a b = fastExpModOpti a b 10
