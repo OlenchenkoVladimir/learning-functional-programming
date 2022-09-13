a5a6a30f1f5f61b13c16d96a2c09c0edb2507fd7
OlenchenkoVladimir <89122417+OlenchenkoVladimir@users.noreply.github.com>
2022-09-13 07:32:47 +0300

    finish modify LastDigiOf.hs

--- LastDigitOf.hs
+++ LastDigitOf.hs
@@ -1,27 +1,6 @@
 module LastDigitOf where
-
-import Data.List()
-import Data.Text (splitOn)
-
--- <func append> -> <empty unit> -> <base elem> -> <exponential> -> <res>
-fastContAppendOp :: Integral a => (b -> b -> b) -> b -> b -> a -> b
-fastContAppendOp _ e _ 0 = e
-fastContAppendOp f e a k
-    | mod k 2 == 1 = f a (fastContAppendOp f e a (k - 1))
-    | otherwise = fastContAppendOp f e (f a a) (div k 2)
-
--- Optimized specified FCOODCMWDBAE algorithm on Monoid <N+, *> with mod operation
--- <base> -> <exp> -> <moder> -> <res>
-fastExpModOpti :: Integral a => a -> a -> a -> a
-fastExpModOpti a k m = fastContAppendOp (\a b -> mod (a * b) m) 1 a k
-
--- Problem 1193 Specified algorithm
--- <A> -> <M> -> <N> -> <res>
-passwordAtNthDay :: Int -> Int -> Int -> Int
-passwordAtNthDay a m n = mod (fastExpModOpti a n m) m
-
-splitAndExtInt :: String -> [Int]
-splitAndExtInt s = map read $ splitOn " " s
-
-lastDigit :: Integer -> Integer -> Integer
-lastDigit a b = fastExpModOpti a b 10
+    
+lastDigit :: Integer -- ^ 
+  -> Integer -- ^ 
+  -> Integer
+lastDigit a b = ((a `rem` 10) ^ ((b - 1) `rem` 4 + 1)) `rem` 10
